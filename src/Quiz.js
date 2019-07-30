3e0b739e9bd0784f42d666dc7a211022464719af
Kelly Cook <kellycook20878@gmail.com>
2019-07-30 13:41:51 -0500

    fixed showNexQuestionHandler

--- src/Quiz.js
+++ src/Quiz.js
@@ -20,7 +20,8 @@ class Quiz extends Component {
             <div>
                 {isQuizEnd ? <QuizEnd /> : 
                 <QuizQuestion quiz_question={quizData.quiz_questions
-                [this.state.quiz_position - 1]} showNextQuestion={this.showNextQuestion.bind(this)} />}
+                [this.state.quiz_position - 1]} showNextQuestionHandler=
+                {this.showNextQuestion.bind(this)} />}
             </div>
         )
     }
